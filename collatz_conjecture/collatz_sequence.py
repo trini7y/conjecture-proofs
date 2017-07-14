3fdf0530a79e5b4dfa9c3a34bec32be428444c15
Okeke Desmond <okekedesmond71@gmail.com>
2017-07-14 16:09:40 +0100

    initial commit

---
+++ collatz_conjecture/collatz_sequence.py
@@ -0,0 +1,19 @@
+print( '''A program to see if the collatz
+conjecture is true \n''')
+print ("======****=================*****======= \n")
+
+number = int(input('Input any number: '))
+def collazt(number):
+  while number > 0:
+    if number % 2 == 0:
+        number = number // 2
+    elif number % 2 == 1:
+      number = (3 * number) + 1
+    if number == 1:
+      print(number)
+      break
+    print (number)
+collazt(number)
+    
+
+                                                          
